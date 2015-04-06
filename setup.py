c73d4eaeaef84f5c968aa5f328d0c7dd7ba0ffeb
Carlo Lobrano <c.lobrano@gmail.com>
2015-04-06 10:24:03 +0200

    Add basic setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name = 'Raffaello',
+        version = '1.1.0',
+        description = 'Raffaello is a command-line output colorizer',
+        author = 'Carlo Lobrano',
+        author_email = 'clobrano@gmail.com',
+        url = 'https://github.com/clobrano/raffaello',
+        long_description = '',
+        licence = 'LICENCE.txt',
+        packages = find_packages())
