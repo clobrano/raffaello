6dd7a6d5c1871a56982e385585ef306314414e17
Carlo Lobrano <carlo.lobrano@telit.com>
2016-11-15 12:07:24 +0100

    Bump version and added RST Readme

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
 
-__version__ = '3.0.1'
+__version__ = '3.0.2'
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='raffaello',
       version=__version__,
@@ -9,6 +17,7 @@ setup(name='raffaello',
       author='Carlo Lobrano',
       author_email='c.lobrano@gmail.com',
       license="MIT",
+      long_description = read('README.rst'),
       url='https://github.com/clobrano/raffaello',
       keywords=['formatter', 'console', 'colorizer'],
       classifiers=[
