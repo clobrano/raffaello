3ea25374303b2b767ee9bd9cf475fc12da40faf9
Carlo Lobrano <carlo.lobrano@telit.com>
2017-01-27 15:44:32 +0100

    Changed test file name

--- tests/test_raffaello.py
+++ tests/test_raffaello.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import unittest
-from raffaello.src.raffaello import Raffaello
-from raffaello.src.raffaello import Commission
+from raffaello import Raffaello
+from raffaello import Commission
 
 class RaffaelloTest(unittest.TestCase):
 
@@ -14,7 +14,7 @@ this=>red
         line = 'this should be in red and This not'
 
         expected = '\x1b[31mthis\x1b[0m should be in red and This not'
-        
+
         raf = Raffaello(c.commission)
         result = raf.paint(line)
         self.assertEqual(expected, result)
@@ -28,11 +28,11 @@ this=>red
         line = 'this should be in red and This too'
 
         expected = '\x1b[31mthis\x1b[0m should be in red and \x1b[31mThis\x1b[0m too'
-        
+
         raf = Raffaello(c.commission)
         result = raf.paint(line)
         self.assertEqual(expected, result)
 
 
-    
+
 
