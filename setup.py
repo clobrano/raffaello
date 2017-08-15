67944d04547ca44f1f0b1288fc4a01598c0b4566
Carlo Lobrano <c.lobrano@gmail.com>
2017-08-15 23:16:52 +0200

    Bumb version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = '3.0.2'
+__version__ = '4.0.0'
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
