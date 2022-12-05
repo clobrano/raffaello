e5fb2028f1ef7615b91bdd10bd4db4a034fcc6ea
Carlo Lobrano <c.lobrano@gmail.com>
2022-12-05 11:00:28 +0100

    reformat setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = '4.0.1'
+__version__ = "4.0.1"
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -11,33 +11,37 @@ __version__ = '4.0.1'
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-if os.path.exists('README.rst'):
+
+if os.path.exists("README.rst"):
     print("file exists")
-    long_description=read('README.rst')
+    long_description = read("README.rst")
 else:
-    long_description=read('README.md')
+    long_description = read("README.md")
 
-setup(name='raffaello',
-      version=__version__,
-      description='Raffaello command-line output colorizer',
-      author='Carlo Lobrano',
-      author_email='c.lobrano@gmail.com',
-      license="MIT",
-      long_description = long_description,
-      url='https://github.com/clobrano/raffaello',
-      keywords=['formatter', 'console', 'colorizer'],
-      classifiers=[
-           "Development Status :: 5 - Production/Stable",
-           "Topic :: Utilities",
-           "Topic :: System :: Logging",
-      ],
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      install_requires=['docopt', ],
-
-      py_modules=['raffaello', 'paint'],
-      entry_points={
-          'console_scripts': ['raffaello=raffaello:main'],
-      },
-      include_package_data=True,
-      )
+setup(
+    name="raffaello",
+    version=__version__,
+    description="Raffaello command-line output colorizer",
+    author="Carlo Lobrano",
+    author_email="c.lobrano@gmail.com",
+    license="MIT",
+    long_description=read("README.rst"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/clobrano/raffaello",
+    keywords=["formatter", "console", "colorizer"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "Topic :: System :: Logging",
+    ],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "docopt",
+    ],
+    py_modules=["raffaello", "paint"],
+    entry_points={
+        "console_scripts": ["raffaello=raffaello:main"],
+    },
+    include_package_data=True,
+)
